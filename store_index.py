6d0eeee89269928a9b927240cbcb5a582544950d
captain-Akshay <akshay.sharma0503@gmail.com>
2024-03-08 17:11:52 +0530

    hehehe
    Signed-off-by: captain-Akshay <akshay.sharma0503@gmail.com>

--- store_index.py
+++ store_index.py
@@ -1,13 +1,12 @@
 from src.helper import load_pdf, text_split, download_hugging_face_embeddings
-from langchain.vectorstores import Pinecone
 import pinecone
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
-PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
+PINECONE_API_KEY = "83d94de1-b537-421a-aaf6-77a5c4be1437"
+PINECONE_API_ENV = "gcp-starter"
 
 # print(PINECONE_API_KEY)
 # print(PINECONE_API_ENV)
@@ -22,7 +21,8 @@ pinecone.init(api_key=PINECONE_API_KEY,
               environment=PINECONE_API_ENV)
 
 
-index_name="medical-bot"
+index_name="medical-chatbot"
 
+from langchain.vectorstores import Pinecone
 #Creating Embeddings for Each of The Text Chunks & storing
 docsearch=Pinecone.from_texts([t.page_content for t in text_chunks], embeddings, index_name=index_name)
